2478ae63ae137771e4117511668e948619c4f0b6
Yuki.N <Syukkic@gmail.com>
2025-04-09 15:46:51 +0800

    style: format SQL

--- migrations/20250409010909_create_ygo_cards_tables.sql
+++ migrations/20250409010909_create_ygo_cards_tables.sql
@@ -1,26 +1,26 @@
 -- Add migration script here
-
-CREATE TABLE IF NOT EXISTS ygo_cards (
-    cid INTEGER PRIMARY KEY,
-    id BIGINT,
-    cn_name TEXT,
-    sc_name TEXT,
-    md_name TEXT,
-    nwbbs_n TEXT,
-    cnocg_n TEXT,
-    jp_ruby TEXT,
-    jp_name TEXT,
-    en_name TEXT,
-    types TEXT,
-    pdesc TEXT,
-    "desc" TEXT,
-    ot INTEGER,
-    setcode BIGINT,
-    "type" INTEGER,
-    atk INTEGER,
-    def INTEGER,
-    level INTEGER,
-    race INTEGER,
-    attribute INTEGER,
-    is_extra BOOLEAN
-);
+CREATE TABLE
+    IF NOT EXISTS ygo_cards (
+        cid INTEGER PRIMARY KEY,
+        id BIGINT,
+        cn_name TEXT,
+        sc_name TEXT,
+        md_name TEXT,
+        nwbbs_n TEXT,
+        cnocg_n TEXT,
+        jp_ruby TEXT,
+        jp_name TEXT,
+        en_name TEXT,
+        types TEXT,
+        pdesc TEXT,
+        "desc" TEXT,
+        ot INTEGER,
+        setcode BIGINT,
+        "type" INTEGER,
+        atk INTEGER,
+        def INTEGER,
+        level INTEGER,
+        race INTEGER,
+        attribute INTEGER,
+        is_extra BOOLEAN
+    );
